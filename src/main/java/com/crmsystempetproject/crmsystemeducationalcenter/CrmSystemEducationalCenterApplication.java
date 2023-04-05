a7e33cb78df96ed64026b7fea75549ef20b0f77e
Nikta Zemlianskikh <rymzar@gmail.com>
2023-04-05 19:37:47 +0300

    first commit

---
+++ src/main/java/com/crmsystempetproject/crmsystemeducationalcenter/CrmSystemEducationalCenterApplication.java
@@ -0,0 +1,13 @@
+package com.crmsystempetproject.crmsystemeducationalcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSystemEducationalCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmSystemEducationalCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crmsystempetproject/crmsystemeducationalcenter/CrmSystemEducationalCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crmsystempetproject.crmsystemeducationalcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSystemEducationalCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
