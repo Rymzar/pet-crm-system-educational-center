9f4347c0ba06f2cbcbb87e7e6b2e3fe5d71e1236
Nikta Zemlianskikh <rymzar@gmail.com>
2023-04-10 20:02:14 +0300

    created exception

---
+++ src/main/java/com/crmsystempetproject/crmsystemeducationalcenter/exception/NoEntityException.java
@@ -0,0 +1,7 @@
+package com.crmsystempetproject.crmsystemeducationalcenter.exception;
+
+public class NoEntityException  extends RuntimeException{
+    public NoEntityException(String messageError) {
+        super(messageError);
+    }
+}
