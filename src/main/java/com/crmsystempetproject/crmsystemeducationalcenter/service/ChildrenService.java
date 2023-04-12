c3a10db32c780dd3a2549713f679a8f35737590b
Nikta Zemlianskikh <rymzar@gmail.com>
2023-04-12 15:22:26 +0300

    refactoring service

--- src/main/java/com/crmsystempetproject/crmsystemeducationalcenter/service/ChildrenService.java
+++ src/main/java/com/crmsystempetproject/crmsystemeducationalcenter/service/ChildrenService.java
@@ -1,14 +1,32 @@
 package com.crmsystempetproject.crmsystemeducationalcenter.service;
 
 import com.crmsystempetproject.crmsystemeducationalcenter.entity.ChildrenEntity;
+import com.crmsystempetproject.crmsystemeducationalcenter.repository.ChildrenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ChildrenService {
-    ChildrenEntity addChildren(ChildrenEntity children);
-    void delete(Long id);
-    Optional<ChildrenEntity> getByName(Long id);
-    ChildrenEntity editChildren(ChildrenEntity children);
-    List<ChildrenEntity> getAll();
+@Service
+public class ChildrenService {
+    @Autowired
+    private ChildrenRepository childrenRepository;
+
+    public List<ChildrenEntity> getAllChildren() {
+        return childrenRepository.findAll();
+    }
+
+    public ChildrenEntity getChidrenById(Long id) {
+        Optional<ChildrenEntity> children = childrenRepository.findById(id);
+        return children.orElse(null);
+    }
+
+    public void saveChildren(ChildrenEntity children) {
+        childrenRepository.save(children);
+    }
+
+    public void deleteChidrenById(Long id) {
+        childrenRepository.deleteById(id);
+    }
 }
